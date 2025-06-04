c9b5cc27db11b51bc78680fb18dcd38ef9c934a8
Santiago Robledo <santiddf9@gmail.com>
2025-06-03 21:31:18 -0300

    agregamos parte de js

---
+++ js/01-variables.js
@@ -0,0 +1,17 @@
+// Comentario de una sola linea
+
+/* 
+Esto es un comentario
+de multiples
+lineas
+*/
+
+//Crear una variable (let - const - var)
+
+let numero1;
+
+let year = 2025
+
+// Mostrar el valor de las variables en js
+console.log("Hola mundo")
+console.log(numero1, year)
