1c3c7d054fc747459576807e62b6edce7356e876
Santiago Robledo <santiddf9@gmail.com>
2025-06-05 21:33:24 -0300

    adelantamos el tp9

---
+++ js/ejercicios/02-TP1-9.js
@@ -0,0 +1,10 @@
+//Escribe un programa que pida una frase y escriba las vocales que aparecen
+//Nota: Tener en cuenta la función length y substring o charAt (developer mozilla)
+
+let frase = prompt("Ingresa una frase")
+
+console.log(frase)
+console.log(frase.length)
+console.log(frase.substring(0, 3))
+console.log(frase.charAt(0))
+console.log(frase.charAt(5))
